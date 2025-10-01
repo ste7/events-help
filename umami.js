fd8edd8f2297facca29754b52a54b596bdedf521
Stefan Babic <stefanbabicbg@gmail.com>
2025-10-01 13:30:01 +0700

    00813e

---
+++ umami.js
@@ -0,0 +1,8 @@
+// Umami Analytics
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://cloud.umami.is/script.js';
+  script.setAttribute('data-website-id', '89a0ea50-9bf3-45cc-94d2-73b6288bdecb');
+  script.defer = true;
+  document.head.appendChild(script);
+})();
